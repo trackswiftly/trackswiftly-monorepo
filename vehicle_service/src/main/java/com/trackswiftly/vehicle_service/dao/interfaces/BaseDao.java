5a0669e009dbc9b728c781331e888799477778fe
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-11 17:21:14 +0100

    add baseDao class that will be implmented by the other repos

---
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/dao/interfaces/BaseDao.java
@@ -0,0 +1,21 @@
+package com.trackswiftly.vehicle_service.dao.interfaces;
+
+import java.util.List;
+
+/***
+ * 
+ * 
+ * Base generic interface , that take entity and its ID (I =ID) , (T = type)
+ */
+public interface BaseDao <T, I>{
+
+    List<T> insertInBatch(List<T> entities);
+    
+    int deleteByIds(List<I> ids);
+    
+    List<T> findByIds(List<I> ids);
+    
+    List<T> findWithPagination(int page, int pageSize);
+    
+    Long count();
+}

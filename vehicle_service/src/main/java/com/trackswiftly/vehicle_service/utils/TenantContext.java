e7ae486b58147d476ff921c967b06efe9d2296b3
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-11 17:22:56 +0100

    Add multi tenantcontext and resolver

---
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/utils/TenantContext.java
@@ -0,0 +1,27 @@
+package com.trackswiftly.vehicle_service.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SuppressWarnings("unused")
+@Slf4j  
+public final class TenantContext {
+    
+    private TenantContext() {}
+
+    private static InheritableThreadLocal<String> currentTenant = new InheritableThreadLocal<>();
+
+    public static void setTenantId(String tenantId) {
+        log.debug("Setting tenantId to " + tenantId);
+        currentTenant.set(tenantId);
+    }
+
+    public static String getTenantId() {
+        return currentTenant.get();
+    }
+
+    public static void clear(){
+        currentTenant.remove();
+    }
+}

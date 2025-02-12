70a6234bb7094b9adfe2bbdd9a285892578eefb0
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-12 17:59:42 +0100

    Add Hints Configruation for native image

---
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/conf/VehicleServiceHintsConfig.java
@@ -0,0 +1,20 @@
+package com.trackswiftly.vehicle_service.conf;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider ;
+
+
+public class VehicleServiceHintsConfig implements RuntimeHintsRegistrar {
+
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        
+        // hints.reflection().registerType(ConnectionProvider.class, MemberCategory.values()) ;
+
+
+        // hints.reflection().registerType(C3P0ConnectionProvider.class, MemberCategory.values()) ;
+    }
+    
+}

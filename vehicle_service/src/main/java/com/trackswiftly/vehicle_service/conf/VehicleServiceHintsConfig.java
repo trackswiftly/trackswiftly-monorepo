ab3b02f0d86630844ca20d875cdc20085c239a35
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-18 16:29:07 +0100

    Add Liquibase Hints Config

--- vehicle_service/src/main/java/com/trackswiftly/vehicle_service/conf/VehicleServiceHintsConfig.java
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/conf/VehicleServiceHintsConfig.java
@@ -1,7 +1,12 @@
 package com.trackswiftly.vehicle_service.conf;
 
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import liquibase.ui.LoggerUIService;
+import liquibase.database.LiquibaseTableNamesFactory;
+import liquibase.report.ShowSummaryGeneratorFactory ;
+
 
 
 public class VehicleServiceHintsConfig implements RuntimeHintsRegistrar {
@@ -9,7 +14,24 @@ public class VehicleServiceHintsConfig implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
 
+        /**
+         * 
+         * registered resources
+         */
         hints.resources().registerPattern("*.pub");
+
+
+        /**
+         * 
+         * registered reflections
+         * 
+         */
+
+        hints.reflection().registerType(LoggerUIService.class, MemberCategory.values()) ;
+        hints.reflection().registerType(LiquibaseTableNamesFactory.class, MemberCategory.values()) ;
+        hints.reflection().registerType(ShowSummaryGeneratorFactory.class, MemberCategory.values()) ;
+
+
     }
     
 }

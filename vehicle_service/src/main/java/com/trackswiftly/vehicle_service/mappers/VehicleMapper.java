37cea1a1bff8808b71ad35fb4d640bb6dd6b6c02
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-21 16:40:45 +0100

    Add toVehicleList that take default group

--- vehicle_service/src/main/java/com/trackswiftly/vehicle_service/mappers/VehicleMapper.java
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/mappers/VehicleMapper.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-
+import com.trackswiftly.vehicle_service.dtos.GroupResponse;
+import com.trackswiftly.vehicle_service.dtos.ModelResponse;
 import com.trackswiftly.vehicle_service.dtos.VehicleRequest;
 import com.trackswiftly.vehicle_service.dtos.VehicleResponse;
+import com.trackswiftly.vehicle_service.dtos.VehicleTypeResponse;
 import com.trackswiftly.vehicle_service.entities.Group;
 import com.trackswiftly.vehicle_service.entities.Model;
 import com.trackswiftly.vehicle_service.entities.Vehicle;
@@ -31,6 +33,12 @@ public class VehicleMapper {
                 .toList();
     }
 
+    public List<Vehicle> toVehicleList(List<VehicleRequest> vehicleRequests , Group defaultGroup) {
+        return vehicleRequests.stream()
+            .map(request -> toVehicle(request, defaultGroup))
+            .toList();
+    }
+
 
 
     public VehicleResponse toVehicleResponse(Vehicle vehicle) {
@@ -40,9 +48,24 @@ public class VehicleMapper {
                 .licensePlate(vehicle.getLicensePlate())
                 .mileage(vehicle.getMileage())
                 .purchaseDate(vehicle.getPurchaseDate())
-                .vehicleType(vehicle.getVehicleType().getId())
-                .model(vehicle.getModel().getId())
-                .vhicleGroup(vehicle.getVehicleGroup().getId())
+                .vehicleType(
+                    VehicleTypeResponse.builder()
+                    .id(vehicle.getVehicleType().getId())
+                    .name(vehicle.getVehicleType().getName())
+                    .build() 
+                )
+                .model(
+                    ModelResponse.builder()
+                    .id(vehicle.getModel().getId())
+                    .name(vehicle.getModel().getName())
+                    .build()
+                )
+                .vhicleGroup(
+                    GroupResponse.builder()
+                    .id(vehicle.getVehicleGroup().getId())
+                    .name(vehicle.getVehicleGroup().getName())
+                    .build()
+                )
                 .updatedAt(vehicle.getUpdatedAt())
                 .createdAt(vehicle.getCreatedAt())
                 .build();
@@ -69,6 +92,36 @@ public class VehicleMapper {
                     Group.builder()
                         .id(vehicleRequest.getVehicleGroupId())
                         .build() 
+                )
+                .build();
+    }
+
+
+
+    public Vehicle toVehicle(VehicleRequest vehicleRequest , Group defaultGroup) {
+        return Vehicle.builder()
+                .vin(vehicleRequest.getVin())
+                .licensePlate(vehicleRequest.getLicensePlate())
+                .mileage(vehicleRequest.getMileage())
+                .purchaseDate(vehicleRequest.getPurchaseDate())
+                .vehicleType(
+                    VehicleType.builder()
+                                .id(vehicleRequest.getVehicleTypeId())
+                                .build()   
+                )
+                .model(
+                    Model.builder()
+                        .id(vehicleRequest.getModelId())
+                        .build()
+                )
+                .vehicleGroup(
+                    vehicleRequest.getVehicleGroupId() != null ?
+
+                    Group.builder()
+                        .id(vehicleRequest.getVehicleGroupId())
+                        .build()
+                    : defaultGroup
+    
                 )
                 .build();
     }

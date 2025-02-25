5722a547361f0717cf5f96a0ac137eabe3be1ebc
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-25 16:15:18 +0000

    Add HomeLocation Dtos

---
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/dtos/HomeLocationRequestDTO.java
@@ -0,0 +1,38 @@
+package com.trackswiftly.vehicle_service.dtos;
+
+import com.trackswiftly.vehicle_service.dtos.interfaces.CreateValidationGroup;
+import com.trackswiftly.vehicle_service.dtos.interfaces.UpdateValidationGroup;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HomeLocationRequestDTO {
+
+    @NotBlank(groups = {CreateValidationGroup.class})
+    @Size(max = 255, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    private String name;
+
+    @NotNull(groups = {CreateValidationGroup.class})
+    @DecimalMin(value = "-90.0", inclusive = true, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @DecimalMax(value = "90.0", inclusive = true, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    private Double latitude;
+
+    @NotNull(groups = {CreateValidationGroup.class})
+    @DecimalMin(value = "-180.0", inclusive = true, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @DecimalMax(value = "180.0", inclusive = true, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    private Double longitude;
+}
---
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/dtos/HomeLocationResponseDTO.java
@@ -0,0 +1,19 @@
+package com.trackswiftly.vehicle_service.dtos;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class HomeLocationResponseDTO {
+    
+    private Long id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}

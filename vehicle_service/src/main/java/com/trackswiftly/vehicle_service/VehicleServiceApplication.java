1d554d71741174ffa38bda3b5e86db304d7c898e
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-25 16:16:00 +0000

    Update VehicleApplication

--- vehicle_service/src/main/java/com/trackswiftly/vehicle_service/VehicleServiceApplication.java
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/VehicleServiceApplication.java
@@ -2,13 +2,11 @@ package com.trackswiftly.vehicle_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 import com.trackswiftly.vehicle_service.conf.VehicleServiceHintsConfig;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 @ImportRuntimeHints(VehicleServiceHintsConfig.class)
 public class VehicleServiceApplication {
 

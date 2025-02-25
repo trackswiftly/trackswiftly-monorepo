846f61da3156db530dd77279c52f4c5fc594d347
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-25 17:33:17 +0000

    Update VehicleMpper

--- vehicle_service/src/main/java/com/trackswiftly/vehicle_service/mappers/VehicleMapper.java
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/mappers/VehicleMapper.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.trackswiftly.vehicle_service.dtos.GroupResponse;
+import com.trackswiftly.vehicle_service.dtos.HomeLocationResponseDTO;
 import com.trackswiftly.vehicle_service.dtos.ModelResponse;
 import com.trackswiftly.vehicle_service.dtos.VehicleRequest;
 import com.trackswiftly.vehicle_service.dtos.VehicleResponse;
 import com.trackswiftly.vehicle_service.dtos.VehicleTypeResponse;
 import com.trackswiftly.vehicle_service.entities.Group;
+import com.trackswiftly.vehicle_service.entities.HomeLocation;
 import com.trackswiftly.vehicle_service.entities.Model;
 import com.trackswiftly.vehicle_service.entities.Vehicle;
 import com.trackswiftly.vehicle_service.entities.VehicleType;
@@ -42,33 +44,47 @@ public class VehicleMapper {
 
 
     public VehicleResponse toVehicleResponse(Vehicle vehicle) {
-        return VehicleResponse.builder()
-                .id(vehicle.getId())
-                .vin(vehicle.getVin())
-                .licensePlate(vehicle.getLicensePlate())
-                .mileage(vehicle.getMileage())
-                .purchaseDate(vehicle.getPurchaseDate())
-                .vehicleType(
-                    VehicleTypeResponse.builder()
-                    .id(vehicle.getVehicleType().getId())
-                    .name(vehicle.getVehicleType().getName())
-                    .build() 
-                )
-                .model(
-                    ModelResponse.builder()
-                    .id(vehicle.getModel().getId())
-                    .name(vehicle.getModel().getName())
-                    .build()
-                )
-                .vhicleGroup(
-                    GroupResponse.builder()
-                    .id(vehicle.getVehicleGroup().getId())
-                    .name(vehicle.getVehicleGroup().getName())
-                    .build()
-                )
-                .updatedAt(vehicle.getUpdatedAt())
-                .createdAt(vehicle.getCreatedAt())
-                .build();
+        VehicleResponse.VehicleResponseBuilder responseBuilder = VehicleResponse.builder()
+            .id(vehicle.getId())
+            .vin(vehicle.getVin())
+            .licensePlate(vehicle.getLicensePlate())
+            .mileage(vehicle.getMileage())
+            .purchaseDate(vehicle.getPurchaseDate())
+            .vehicleType(
+                VehicleTypeResponse.builder()
+                .id(vehicle.getVehicleType().getId())
+                .name(vehicle.getVehicleType().getName())
+                .build()
+            )
+            .model(
+                ModelResponse.builder()
+                .id(vehicle.getModel().getId())
+                .name(vehicle.getModel().getName())
+                .build()
+            )
+            .vhicleGroup(
+                GroupResponse.builder()
+                .id(vehicle.getVehicleGroup().getId())
+                .name(vehicle.getVehicleGroup().getName())
+                .build()
+            );
+
+        HomeLocation homeLocation = vehicle.getHomeLocation();
+        if (homeLocation != null) {
+            responseBuilder.homeLocation(
+                HomeLocationResponseDTO.builder()
+                .id(homeLocation.getId())
+                .name(homeLocation.getName())
+                .latitude(homeLocation.getLatitude())
+                .longitude(homeLocation.getLongitude())
+                .build()
+            );
+        }
+
+        return responseBuilder
+            .updatedAt(vehicle.getUpdatedAt())
+            .createdAt(vehicle.getCreatedAt())
+            .build();
     }
     
 
@@ -92,6 +108,11 @@ public class VehicleMapper {
                     Group.builder()
                         .id(vehicleRequest.getVehicleGroupId())
                         .build() 
+                )
+                .homeLocation(
+                    HomeLocation.builder()
+                    .id(vehicleRequest.getHomeLocationId())
+                    .build()
                 )
                 .build();
     }

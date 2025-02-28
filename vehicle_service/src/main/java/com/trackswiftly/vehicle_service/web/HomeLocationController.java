225a1a24b29f1aeaca91ad580af6a6c8d81968cd
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-02-28 11:57:00 +0000

    Update HomeLocationController

--- vehicle_service/src/main/java/com/trackswiftly/vehicle_service/web/HomeLocationController.java
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/web/HomeLocationController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.trackswiftly.vehicle_service.dtos.GroupRequest;
 import com.trackswiftly.vehicle_service.dtos.HomeLocationRequestDTO;
 import com.trackswiftly.vehicle_service.dtos.HomeLocationResponseDTO;
 import com.trackswiftly.vehicle_service.dtos.OperationResult;

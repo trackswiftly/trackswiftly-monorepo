c5340fbe5ac31b740be272e969061215341c8184
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-03-19 15:32:40 +0000

    Add Group Dtos

---
+++ client_service/src/main/java/com/trackswiftly/client_service/dtos/GroupResponse.java
@@ -0,0 +1,23 @@
+package com.trackswiftly.client_service.dtos;
+
+import java.time.Instant;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GroupResponse{
+    
+    private Long id ;
+
+    private String name ;
+
+    private String description ;
+
+
+    private Instant createdAt ;
+
+    private Instant updatedAt ;
+}

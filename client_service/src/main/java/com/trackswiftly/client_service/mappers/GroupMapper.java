d86622ffa090c5ecdc80ce92c1006f2fbc54666a
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-03-19 15:44:18 +0000

    Add GroupMapper

---
+++ client_service/src/main/java/com/trackswiftly/client_service/mappers/GroupMapper.java
@@ -0,0 +1,59 @@
+package com.trackswiftly.client_service.mappers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.trackswiftly.client_service.dtos.GroupRequest;
+import com.trackswiftly.client_service.dtos.GroupResponse;
+import com.trackswiftly.client_service.entities.Group;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Component
+public class GroupMapper {
+
+
+
+    public List<GroupResponse> toGroupResponseList(List<Group> groups) {
+
+        log.debug("group :{} " , groups);
+
+        List<GroupResponse> groupResponses = groups.stream()
+                                            .map(this::toGroupResponse)
+                                            .toList() ;
+        
+        log.debug("group responses :{} " , groupResponses);
+        return groupResponses;
+    }
+
+
+    public List<Group> toGroupList(List<GroupRequest> groupRequests) {
+        return groupRequests.stream()
+                .map(this::toGroup)
+                .toList();
+    }
+    
+
+
+
+    public GroupResponse toGroupResponse(Group group) {
+        return GroupResponse.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .description(group.getDescription())
+                .updatedAt(group.getUpdatedAt())
+                .createdAt(group.getCreatedAt())
+                .build();
+    }
+    
+
+    public Group toGroup(GroupRequest groupRequest) {
+        return Group.builder()
+                .name(groupRequest.getName())
+                .description(groupRequest.getDescription())
+                .build();
+    }
+}

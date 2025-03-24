8ef32c5c1ee04b9214118d9a6db6625e6f80943d
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-03-24 16:42:29 +0000

    Update EntityValidator

--- client_service/src/main/java/com/trackswiftly/client_service/validators/EntityValidator.java
+++ client_service/src/main/java/com/trackswiftly/client_service/validators/EntityValidator.java
@@ -1,18 +1,11 @@
 package com.trackswiftly.client_service.validators;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Function;
 
 import org.springframework.stereotype.Component;
 
-import com.trackswiftly.client_service.exception.UnableToProccessIteamException;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
 import lombok.extern.slf4j.Slf4j;
 
 @Component

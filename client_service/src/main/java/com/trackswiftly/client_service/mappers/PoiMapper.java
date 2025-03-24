9289f876e48e3b0db9684dc193bff7486940e87d
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-03-24 16:42:18 +0000

    Add PoiMapper

---
+++ client_service/src/main/java/com/trackswiftly/client_service/mappers/PoiMapper.java
@@ -0,0 +1,91 @@
+package com.trackswiftly.client_service.mappers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.trackswiftly.client_service.dtos.GroupResponse;
+import com.trackswiftly.client_service.dtos.PoiRequest;
+import com.trackswiftly.client_service.dtos.PoiResponse;
+import com.trackswiftly.client_service.dtos.PoiTypeResponse;
+import com.trackswiftly.client_service.entities.Group;
+import com.trackswiftly.client_service.entities.Poi;
+import com.trackswiftly.client_service.entities.PoiType;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Component
+public class PoiMapper {
+
+
+
+    public List<PoiResponse> toPoiResponseList(List<Poi> pois) {
+
+
+        List<PoiResponse> poiResponses = pois.stream()
+                                            .map(this::toPoiResponse)
+                                            .toList() ;
+        
+        log.debug("PoiResponses :{} " , poiResponses);
+        return poiResponses;
+    }
+
+
+    public List<Poi> toPoiList(List<PoiRequest> poiRequests) {
+        return poiRequests.stream()
+                .map(this::toPoi)
+                .toList();
+    }
+    
+
+
+
+    public PoiResponse toPoiResponse(Poi poi) {
+        return PoiResponse.builder()
+                .id(poi.getId())
+                .name(poi.getName())
+                .group(
+                        GroupResponse.builder()
+                                .id(poi.getGroup().getId())
+                                .name(poi.getGroup().getName())
+                                .build()
+                )
+                .poiTypeResponse(
+                        PoiTypeResponse.builder()
+                                .id(poi.getType().getId())
+                                .name(poi.getType().getName())
+                                .build()
+                )
+                .address(poi.getAddress())
+                .latitude(poi.getLatitude())
+                .longitude(poi.getLongitude())
+                .payload(poi.getPayload())
+                .updatedAt(poi.getUpdatedAt())
+                .createdAt(poi.getCreatedAt())
+                .build();
+    }
+    
+
+    public Poi toPoi(PoiRequest poiRequest) {
+        return Poi.builder()
+                .name(poiRequest.getName())
+                .tenantId(poiRequest.getTenantId())
+                .group(
+                        Group.builder()
+                                .id(poiRequest.getGroupId())
+                                .build()
+                )
+                .type(
+                        PoiType.builder()
+                                .id(poiRequest.getTypeId())
+                                .build()
+                )
+                .address(poiRequest.getAddress())
+                .latitude(poiRequest.getLatitude())
+                .longitude(poiRequest.getLongitude())
+                .payload(poiRequest.getPayload())
+                .build();
+    }
+}

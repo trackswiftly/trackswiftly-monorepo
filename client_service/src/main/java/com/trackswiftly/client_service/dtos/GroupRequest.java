b34efa675ca2137d9a6569d82ef3c73681e4c86b
ObaidOnCall <abdellatif.hour.web@gmail.com>
2025-03-26 10:52:56 +0000

    Update Validation Pattern of the name filed in GroupRequest

--- client_service/src/main/java/com/trackswiftly/client_service/dtos/GroupRequest.java
+++ client_service/src/main/java/com/trackswiftly/client_service/dtos/GroupRequest.java
@@ -23,8 +23,10 @@ public class GroupRequest {
 
     @NotNull(groups = CreateValidationGroup.class, message = "Name is required for create operation")
     @NotBlank(groups = {CreateValidationGroup.class}, message = "Name must not be blank")
-    @Pattern(regexp = "^[A-Za-z]+$", message = "Name must only contain alphabetic characters", groups = { CreateValidationGroup.class, UpdateValidationGroup.class })
-    private String name ;
+    @Pattern(regexp = "^[\\p{L}\\p{IsHan}]+$", message = "Name must only contain alphabetic characters", 
+            groups = { CreateValidationGroup.class, UpdateValidationGroup.class })
+    private String name;
+
 
     @Size(min = 20, max = 255, message = "Description must be between 20 and 255 characters", groups = { CreateValidationGroup.class, UpdateValidationGroup.class })
     @Pattern(regexp = "^[A-Za-z0-9\\s.,!?;:'\"()-]+$", message = "Description contains invalid characters", groups = { CreateValidationGroup.class, UpdateValidationGroup.class })

cc9ba4d7bd0d86ec1a42a92d69f3625f099c2b6f
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-03 16:36:08 +0000

    Update ClientServiceApplication

--- client_service/src/main/java/com/trackswiftly/client_service/ClientServiceApplication.java
+++ client_service/src/main/java/com/trackswiftly/client_service/ClientServiceApplication.java
@@ -2,9 +2,13 @@ package com.trackswiftly.client_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+import com.trackswiftly.client_service.conf.ClientHintsConfig;
 
 
 @SpringBootApplication
+@ImportRuntimeHints(ClientHintsConfig.class)
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {

45e4fab299258a6d4e75db7152ba3f22bca99bf7
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-03 16:06:48 +0000

    Update PoiTypeRequest

--- client_service/src/main/java/com/trackswiftly/client_service/dtos/PoiTypeRequest.java
+++ client_service/src/main/java/com/trackswiftly/client_service/dtos/PoiTypeRequest.java
@@ -13,10 +13,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PoiTypeRequest {
     
     @NotNull(groups = CreateValidationGroup.class, message = "Name is required for create operation")

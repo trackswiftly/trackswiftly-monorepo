e67b43fdb29cc17bcc33cc71c999caa61de23034
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-03 16:36:18 +0000

    Add ClientHintsConfig

---
+++ client_service/src/main/java/com/trackswiftly/client_service/conf/ClientHintsConfig.java
@@ -0,0 +1,58 @@
+package com.trackswiftly.client_service.conf;
+
+
+import java.util.UUID;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+
+import com.trackswiftly.client_service.utils.TenantStatementInspector;
+
+import liquibase.ui.LoggerUIService;
+import liquibase.database.LiquibaseTableNamesFactory;
+import liquibase.report.ShowSummaryGeneratorFactory ;
+import liquibase.parser.SqlParserFactory;
+
+import liquibase.changelog.visitor.ValidatingVisitorGeneratorFactory ;
+import liquibase.changelog.FastCheckService ;
+
+
+public class ClientHintsConfig implements RuntimeHintsRegistrar{
+
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        
+        /**
+         * 
+         * registered resources
+         */
+        hints.resources().registerPattern("*.pub");
+
+
+
+        /**
+         * 
+         * registered reflections
+         * 
+         */
+
+        hints.reflection().registerType(LoggerUIService.class, MemberCategory.values()) ;
+        hints.reflection().registerType(LiquibaseTableNamesFactory.class, MemberCategory.values()) ;
+        hints.reflection().registerType(ShowSummaryGeneratorFactory.class, MemberCategory.values()) ;
+        hints.reflection().registerType(SqlParserFactory.class, MemberCategory.values()) ;
+
+
+        hints.reflection().registerType(TenantStatementInspector.class, MemberCategory.values()) ;
+        hints.reflection().registerType(UUID[].class, MemberCategory.values()) ;
+
+        /**
+         * 
+        * Liquibase
+        */
+        hints.reflection().registerType(ValidatingVisitorGeneratorFactory.class, MemberCategory.values()) ;
+        hints.reflection().registerType(FastCheckService.class, MemberCategory.values()) ;
+
+    }
+    
+}

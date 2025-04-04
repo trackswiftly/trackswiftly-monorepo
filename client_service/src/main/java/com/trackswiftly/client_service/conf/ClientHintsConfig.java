079d9dc48517bf7bf195bae066a2fb2baa008dab
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-04 11:09:47 +0000

    Update ClientHintsConfig add swagger hints

--- client_service/src/main/java/com/trackswiftly/client_service/conf/ClientHintsConfig.java
+++ client_service/src/main/java/com/trackswiftly/client_service/conf/ClientHintsConfig.java
@@ -9,6 +9,13 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
 
 import com.trackswiftly.client_service.utils.TenantStatementInspector;
 
+import io.swagger.v3.core.jackson.mixin.Schema31Mixin;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.JsonSchema;
+import io.swagger.v3.oas.models.media.Schema;
 import liquibase.ui.LoggerUIService;
 import liquibase.database.LiquibaseTableNamesFactory;
 import liquibase.report.ShowSummaryGeneratorFactory ;
@@ -16,6 +23,7 @@ import liquibase.parser.SqlParserFactory;
 
 import liquibase.changelog.visitor.ValidatingVisitorGeneratorFactory ;
 import liquibase.changelog.FastCheckService ;
+import com.fasterxml.jackson.databind.BeanDescription;
 
 
 public class ClientHintsConfig implements RuntimeHintsRegistrar{
@@ -52,6 +60,24 @@ public class ClientHintsConfig implements RuntimeHintsRegistrar{
         */
         hints.reflection().registerType(ValidatingVisitorGeneratorFactory.class, MemberCategory.values()) ;
         hints.reflection().registerType(FastCheckService.class, MemberCategory.values()) ;
+
+
+
+        /**
+         * 
+         * swagger config
+         */
+
+        hints.reflection().registerType(
+            Schema31Mixin.TypeSerializer.class ,
+            MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
+            MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+            MemberCategory.DECLARED_CLASSES
+        ) ;
+
+
+        hints.reflection().registerType(JsonSchema.class, MemberCategory.values());
+        hints.reflection().registerType(BeanDescription.class, MemberCategory.values());
 
     }
     

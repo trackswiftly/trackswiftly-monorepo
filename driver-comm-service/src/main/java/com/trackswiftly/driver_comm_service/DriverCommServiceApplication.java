447d54960703ae75a1039207cb9b37d433c6b4c8
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-17 12:15:56 +0100

    Init the deriver comm service

---
+++ driver-comm-service/src/main/java/com/trackswiftly/driver_comm_service/DriverCommServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trackswiftly.driver_comm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverCommServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverCommServiceApplication.class, args);
+	}
+
+}
---
+++ driver-comm-service/src/test/java/com/trackswiftly/driver_comm_service/DriverCommServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackswiftly.driver_comm_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverCommServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

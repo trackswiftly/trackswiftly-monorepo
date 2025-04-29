a9acb4787326c495130738d5edf6bf2319181753
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-29 16:47:36 +0100

    Add JobDto

---
+++ client_service/src/main/java/com/trackswiftly/client_service/dtos/routing/JobDto.java
@@ -0,0 +1,67 @@
+package com.trackswiftly.client_service.dtos.routing;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.trackswiftly.client_service.dtos.validators.ValidLocation;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobDto {
+
+    private Integer id;
+
+    private String description;
+
+    @Size(min = 2, max = 2 , message = "Location must contain exactly 2 elements [longitude, latitude]")
+    @ValidLocation
+    private List<Double> location;
+
+
+    @JsonProperty("location_index")
+    private List<Integer> locationIndex;
+
+    @Builder.Default
+    private Integer setup = 0;
+
+    @Builder.Default
+    private Integer service = 0;
+
+
+    private List<Integer> delivery;
+
+    private List<Integer> pickup;
+
+    private List<Integer> skills;
+
+
+    @Min(0)
+    @Max(100)
+    @Builder.Default
+    private Integer priority = 0;
+
+
+    @JsonProperty("time_windows")
+    private List<TimeWindow> timeWindows;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class TimeWindow {
+        private Integer start;
+        private Integer end;
+    }
+    
+}

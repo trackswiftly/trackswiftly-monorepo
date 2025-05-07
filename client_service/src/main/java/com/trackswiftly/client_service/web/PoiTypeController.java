86b3928edb0835f9c07ea5b7aa0a99dbf2475c92
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-07 12:15:59 +0100

    Update PoiTYpeController.java

--- client_service/src/main/java/com/trackswiftly/client_service/web/PoiTypeController.java
+++ client_service/src/main/java/com/trackswiftly/client_service/web/PoiTypeController.java
@@ -1,5 +1,7 @@
 package com.trackswiftly.client_service.web;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.trackswiftly.client_service.dtos.PoiTypeRequest;
 import com.trackswiftly.client_service.dtos.PoiTypeResponse;
@@ -132,4 +133,16 @@ public class PoiTypeController {
         return ResponseEntity.ok(result);
     }
     
+
+
+    @GetMapping("/search")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER') or hasRole('ROLE_DISPATCHER')")
+    public ResponseEntity<List<PoiTypeResponse>> search(
+        @RequestParam String query
+    ) {
+        String decodedQuery = URLDecoder.decode(query, StandardCharsets.UTF_8);
+        return ResponseEntity.ok(
+            poiTypeService.search(decodedQuery)
+        );
+    }
 }

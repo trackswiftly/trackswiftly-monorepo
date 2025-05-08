734d157ab158f9463945538fba84d70104d62275
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-08 15:55:48 +0100

    Add VehicleTypeEnum.java

---
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/enums/VehicleTypeEnum.java
@@ -0,0 +1,13 @@
+package com.trackswiftly.vehicle_service.enums;
+
+public enum VehicleTypeEnum {
+    AUTO,
+    BICYCLE,
+    PEDESTRIAN,
+    MOTORCYCLE,
+    MOTOR_SCOOTER,
+    TAXI,
+    HOV,
+    TRUCK,
+    BUS
+}

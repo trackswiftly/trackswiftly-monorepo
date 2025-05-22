7e247654e4320e1f83f9fac2b12235582adf72c7
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-22 13:01:21 +0100

    Update VehicleServiceHintsConfig

--- vehicle_service/src/main/java/com/trackswiftly/vehicle_service/conf/VehicleServiceHintsConfig.java
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/conf/VehicleServiceHintsConfig.java
@@ -1,13 +1,17 @@
 package com.trackswiftly.vehicle_service.conf;
 
 import java.util.UUID;
+import java.util.function.Function;
 
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 
+import com.fasterxml.jackson.databind.BeanDescription;
 import com.trackswiftly.vehicle_service.utils.TenantStatementInspector;
 
+import io.swagger.v3.core.jackson.mixin.Schema31Mixin;
+import io.swagger.v3.oas.models.media.JsonSchema;
 import liquibase.ui.LoggerUIService;
 import liquibase.database.LiquibaseTableNamesFactory;
 import liquibase.report.ShowSummaryGeneratorFactory ;
@@ -50,6 +54,27 @@ public class VehicleServiceHintsConfig implements RuntimeHintsRegistrar {
          */
         hints.reflection().registerType(ValidatingVisitorGeneratorFactory.class, MemberCategory.values()) ;
         hints.reflection().registerType(FastCheckService.class, MemberCategory.values()) ;
+
+
+
+         /**
+         * 
+         * swagger config
+         */
+
+        hints.reflection().registerType(
+            Schema31Mixin.TypeSerializer.class ,
+            MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
+            MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+            MemberCategory.DECLARED_CLASSES
+        ) ;
+
+
+        hints.reflection().registerType(JsonSchema.class, MemberCategory.values());
+        hints.reflection().registerType(BeanDescription.class, MemberCategory.values());
+
+        // Add this if you still see reflection errors for Function itself
+        hints.reflection().registerType(Function[].class, MemberCategory.values());
 
     }
     

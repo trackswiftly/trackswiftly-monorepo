b709713eb1109f0627284c6696e91d381ac4ddf2
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-23 12:02:29 +0100

    Enhance Capacity class with constructors and improve code structure

--- vehicle_service/src/main/java/com/trackswiftly/vehicle_service/enums/Capacity.java
+++ vehicle_service/src/main/java/com/trackswiftly/vehicle_service/enums/Capacity.java
@@ -1,16 +1,23 @@
 package com.trackswiftly.vehicle_service.enums;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Capacity {
     
 
     private Double maxWeightKg;
+
+    @Column(name = "max_volume_m3")
     private Double maxVolumeM3;
     private Integer maxBoxes;
     private Integer maxPallets;

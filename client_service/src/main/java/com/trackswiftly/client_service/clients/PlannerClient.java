e0c5dc388f13fa823bb53093d217cefabe157d2d
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-28 15:16:22 +0100

    Remove unused import statement in PlannerClient

--- client_service/src/main/java/com/trackswiftly/client_service/clients/PlannerClient.java
+++ client_service/src/main/java/com/trackswiftly/client_service/clients/PlannerClient.java
@@ -1,7 +1,6 @@
 package com.trackswiftly.client_service.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 

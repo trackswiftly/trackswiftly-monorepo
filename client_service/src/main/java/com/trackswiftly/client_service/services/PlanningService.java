df5cd2ba58b4e3db0f13f1a758aad93ee09dafdf
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-28 15:15:59 +0100

    Implement PlanningService class for managing planning entities and batch creation

---
+++ client_service/src/main/java/com/trackswiftly/client_service/services/PlanningService.java
@@ -0,0 +1,58 @@
+package com.trackswiftly.client_service.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trackswiftly.client_service.clients.PlannerClient;
+import com.trackswiftly.client_service.dao.repositories.PlanningRepo;
+import com.trackswiftly.client_service.dao.repositories.PoiRepo;
+import com.trackswiftly.client_service.dtos.GroupRequest;
+import com.trackswiftly.client_service.dtos.GroupResponse;
+import com.trackswiftly.client_service.dtos.routing.PlanningRequest;
+import com.trackswiftly.client_service.dtos.routing.PlanningResponse;
+import com.trackswiftly.client_service.entities.PlanningEnitity;
+import com.trackswiftly.client_service.mappers.PoiMapper;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Slf4j
+@Service
+@Transactional
+public class PlanningService {
+
+    private final PlanningRepo planningRepo;
+
+    private final PlannerClient plannerClient;
+
+
+    @Autowired
+    PlanningService(
+        PlanningRepo planningRepo ,
+        PlannerClient plannerClient
+    ) {
+        this.planningRepo = planningRepo;
+        this.plannerClient = plannerClient;
+    }
+
+
+
+    public List<PlanningEnitity> createPlanningEnitities(List<PlanningRequest> planningRequests) {
+        log.info("Creating groups in batch...");
+
+        PlanningResponse response = plannerClient.createOptimizationPlan(planningRequests.get(0));
+        
+        return  planningRepo.insertInBatch(
+                    List.of(
+                        PlanningEnitity.builder()
+                            .planningData(response)
+                            .build()
+                    )
+                ) ;
+    }
+
+}
